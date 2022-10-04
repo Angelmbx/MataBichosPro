7a1f09ad49f65bd873b53b0eb118b44f584812c1
Ángel Miranda Blanco <a21angelmb@ATEIS.LOCAL>
2022-10-04 18:19:39 +0200

    commit desde clase

--- app/src/main/java/com/example/prueba1/MainActivity.java
+++ app/src/main/java/com/example/prueba1/MainActivity.java
@@ -4,12 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    int contador =0;
-    TextView t1;
+    int contador = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +18,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button b1 = findViewById(R.id.button);
-        b1.setText("¡Un bicho!");
+        TextView txt = findViewById(R.id.textView);
+        txt.setText("¡Mátalo!");
+        ImageView imageView = findViewById(R.id.imageView);
+        imageView.setVisibility(View.INVISIBLE);
 
-    }
 
-    public void cont (View v){
+        b1.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-        contador++;
-        t1 = findViewById(R.id.textView);
-        t1.setText("Has matado "+contador+" bichos.");
+
+                contador++;
+
+                txt.setText("Has matado "+ contador + " bichos.");
+
+                if (contador>14){
+                    b1.setVisibility(View.INVISIBLE);
+
+                    txt.setText("Los has matado a todos D:");
+
+
+                    imageView.setVisibility(View.VISIBLE);
+
+                }
+
+
+
+
+            }
+        });
+
 
     }
 }
